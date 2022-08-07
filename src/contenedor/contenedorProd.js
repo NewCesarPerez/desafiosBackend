a2462519677a9dc5a4d550c7c6431dba626ebf54
César Pérez <77213082+NewCesarPerez@users.noreply.github.com>
2022-08-07 17:46:46 -0300

    Delete src directory

--- src/contenedor/contenedorProd.js
+++
@@ -1,8 +0,0 @@
-const knexBaseContainer = require('./knexBase')
-const mySqlConfig=require('../db/db_config_mysql')
-class ContenedorProd extends knexBaseContainer {
-  constructor(configKnex, tableName) {
-    super(configKnex, tableName)
-  }
-}
-module.exports = new ContenedorProd(mySqlConfig, 'productos');
