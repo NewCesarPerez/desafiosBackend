dea19c907edcdf9a5fcd93ef50b29c3fb9a2817f
César Pérez <77213082+NewCesarPerez@users.noreply.github.com>
2022-09-07 23:18:54 -0300

    Add files via upload

---
+++ desafios/Desafio_BalanceCarga/config/config.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+dotenv.config()
+ 
+import ARGS from "../yargs/configuration.js";
+
+export default {
+    mongodb: {
+        URI: process.env.MONGO_URL
+    },
+    session: {
+        SECRET: process.env.SECRET
+    },
+    port: ARGS.puerto
+}
