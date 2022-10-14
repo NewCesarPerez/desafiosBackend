c53a3f3454a2a7d6b1bdd810f71bf7753a7baeeb
César Pérez <77213082+NewCesarPerez@users.noreply.github.com>
2022-10-14 17:02:43 -0300

    Delete desafios/Desafio_Capas directory

--- desafios/Desafio_Capas/src/daos/productos/productoDaoFB.js
+++
@@ -1,8 +0,0 @@
-import ContenedorFB from "../../contenedores/contenedorFB.js";
-
-export class ProductoDaoFirebase extends ContenedorFB {
-  constructor() {
-    super("productos");
-  }
-}
-export default ProductoDaoFirebase;
